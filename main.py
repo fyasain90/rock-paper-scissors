0e04f2f138712cd84930476fcb16f957c2ea0554
Feras-Yasain <fyasain.dev@gmail.com>
2021-09-08 15:56:44 +0300

    Feat: Add codes
    Building the program

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-# coding=utf-8
-# This is a sample Python script.
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
+
+if user_choice < 0 or user_choice >= 3:
+    print("You typed an invalid number")
+else:
+    print("Your choice: ")
+    print(game_images[user_choice])
+    computer_choice = random.randint(0, 2)
+    print("Computer chose: ")
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose!")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice > user_choice:
+        print("You lose!")
+    elif user_choice == computer_choice:
+        print("Its a Draw")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
